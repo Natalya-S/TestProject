44a53ffb78723d840c6f7897e85237f301ec59ff
Natalya-S <nsozykina@gmail.com>
2016-10-17 10:23:17 +0600

    Added code for Update record

--- TestDBEF/Program.cs
+++ TestDBEF/Program.cs
@@ -23,27 +23,34 @@ namespace TestDBEF
             Console.WriteLine("Объект успешно сохранен");
         }
 
-		static void Main(string[] args)
+        static void Main(string[] args)
         {
             using (UserContext db = new UserContext())
             {
                 var users = db.Users;
                 ListUsers(db, "Users (initial data):");
+                
+                //delete all records from DB:
+                foreach (var r in users)
+                {
+                    users.Remove(r);
+                }
+                db.SaveChanges();
+                Console.WriteLine("Records are deleted. Count = {0}", db.Users.Count());
 
-				//delete all records from DB:
-				foreach (var r in users)
-				{
-					users.Remove(r);
-				}
-				db.SaveChanges();
-				Console.WriteLine("Records are deleted. Count = {0}", db.Users.Count());
-
-
-				// создаем два объекта User
-				InsertRecord(db, new User { Name = "Tom", Age = 33 });
+                // создаем два объекта User
+                InsertRecord(db, new User { Name = "Tom", Age = 33 });
                 InsertRecord(db, new User { Name = "Sam", Age = 26 });
 
-                
+                ListUsers(db, "Users (after insert):");
+
+                //update
+                var userForUpdate = db.Users.Where(n => n.Name == "Tom").FirstOrDefault();
+                userForUpdate.Age = 100;
+                db.SaveChanges();
+
+                ListUsers(db, "Users (after update):");
+
             }
             Console.Read();
         }
