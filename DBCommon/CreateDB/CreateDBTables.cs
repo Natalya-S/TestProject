c0bbda35072456382bc40fa38051df26af555041
Natalya-S <nsozykina@gmail.com>
2016-10-17 18:51:31 +0600

    Create DB and Permission table

---
+++ DBCommon/CreateDB/CreateDBTables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DB
+{
+    class CreateDBTables
+    {
+        static void Main(string[] args)
+        {
+            using (PermissionContext db = new PermissionContext())
+            {
+                db.Database.CreateIfNotExists();
+                //Console.WriteLine("Permissions table is created");
+            }
+        }
+    }
+}
---
+++ DBCommon/CreateDB/PermissionContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace DB
+{
+    class PermissionContext: DbContext
+    {
+        public PermissionContext() : base("DBConnection")
+        { }
+
+        public DbSet<Permission> Permissions { get; set; }
+    }
+}
